e6b6061dec1d209383c1aed48d146d71cc87f2b2
Bas Visscher <bas.visscher24@gmail.com>
2022-08-06 12:11:26 +0200

    hdmi test not working

---
+++ test/src/gowin_rpll/gowin_rpll_tmp.vhd
@@ -0,0 +1,25 @@
+--Copyright (C)2014-2022 Gowin Semiconductor Corporation.
+--All rights reserved.
+--File Title: Template file for instantiation
+--GOWIN Version: V1.9.8.05
+--Part Number: GW1NR-LV9QN88PC6/I5
+--Device: GW1NR-9C
+--Created Time: Fri Aug 05 22:44:30 2022
+
+--Change the instance name and port connections to the signal names
+----------Copy here to design--------
+
+component Gowin_rPLL
+    port (
+        clkout: out std_logic;
+        clkin: in std_logic
+    );
+end component;
+
+your_instance_name: Gowin_rPLL
+    port map (
+        clkout => clkout_o,
+        clkin => clkin_i
+    );
+
+----------Copy end-------------------
